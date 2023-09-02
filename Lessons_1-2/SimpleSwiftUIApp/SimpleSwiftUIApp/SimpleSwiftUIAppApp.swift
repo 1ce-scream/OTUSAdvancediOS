a6434697271b6af07ce60bbdee597f6cdd88784c
Vitaliy Viktorovich <po4tavela@gmail.com>
2023-09-02 17:38:28 +0300

    Initial app lesson 1

---
+++ Lessons_1-2/SimpleSwiftUIApp/SimpleSwiftUIApp/SimpleSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSwiftUIAppApp.swift
+//  SimpleSwiftUIApp
+//
+//  Created by Vitaliy Talalay on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
