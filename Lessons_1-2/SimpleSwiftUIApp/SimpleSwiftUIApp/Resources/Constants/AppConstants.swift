c0cb2aa42ce98e71ba29bb86c8158e4ba2e618e4
Vitaliy Viktorovich <po4tavela@gmail.com>
2023-09-03 15:02:10 +0300

    Добавлены константы для приложения

---
+++ Lessons_1-2/SimpleSwiftUIApp/SimpleSwiftUIApp/Resources/Constants/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  SimpleSwiftUIApp
+//
+//  Created by Vitaliy Talalay on 03.09.2023.
+//
+
+import Foundation
+
+enum AppConstants {
+    static let cornerRadius: CGFloat = 16
+    static let horizontalPadding: CGFloat = 16
+    static let verticalPadding: CGFloat = 12
+    static let lineWidth: CGFloat = 2
+}
